b463d4a45fb546bf07c4a6d8a41568dc732b3dc7
Rich Alaba <richalaba@gmail.com>
2022-04-29 10:37:33 +0800

    datetime

--- back/main2.py
+++ back/main2.py
@@ -1,10 +1,17 @@
 from flask import Flask, jsonify, request
+from flask_restx import Api, Resource, reqparse
 import mysql.connector
 import csv
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask("Procurements API")
+api = Api(app)
+
+#flask_app = Flask(__name__)
+#app = Api(app = flask_app)
+#name_space = app.namespace('main', description='Main APIs')
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:theadmin@localhost/obback'
 db = SQLAlchemy(app)
 
@@ -28,104 +35,115 @@ class Procurements(db.Model):
     def __repr__(self):
         return '<Procurements {}>'.format(self.id)
 
+parser = reqparse.RequestParser()
+parser.add_argument('page', type=str, help='Current Page')
+parser.add_argument('pageSize', type=str, help='Number of item per page')
+parser.add_argument('agency', type=str, help='Agency Name')
+parser.add_argument('supplier', type=str, help='Supplier Name')
+parser.add_argument('keyword', type=str, help='Keywords on the Contracts')
 
-@app.get('/procurements')
-def procurements():
-    page = request.args.get('page', 1, type=int)
-    pageSize = request.args.get('pageSize', 10, type=int)
-    agency = request.args.get('agency','',type=str)
-    supplier = request.args.get('supplier','',type=str)
-    keyword = request.args.get('keyword','',type=str)
-    procurements = Procurements.query.filter(Procurements.agency.like('%'+agency+'%')).filter(Procurements.supplier_name.like('%'+supplier+'%')).filter(Procurements.tender_description.like('%'+keyword+'%')).paginate(page, pageSize, False)
-    procurementsItem = procurements.items
-    data = []
-    for row in procurementsItem:
-        temp = {
-            "tenderNo": row.tender_no,
-            "tenderDescription": row.tender_description,
-            "agency": row.agency,
-            "awardDate": row.award_date,
-            "tenderDetailStatus": row.tender_detail_status,
-            "supplierName": row.supplier_name,
-            "awardedAmt": row.awarded_amt,
-            "yearAwarded": row.award_date
+@api.route("/procurements")
+class procurementsDelete(Resource):
+    @api.doc(parser=parser)
+    def delete(self):
+        page = request.args.get('page', 1, type=int)
+        pageSize = request.args.get('pageSize', 10, type=int)
+        agency = request.args.get('agency','',type=str)
+        supplier = request.args.get('supplier','',type=str)
+        keyword = request.args.get('keyword','',type=str)
+        procurements = Procurements.query.filter(Procurements.agency.like('%'+agency+'%')).filter(Procurements.supplier_name.like('%'+supplier+'%')).filter(Procurements.tender_description.like('%'+keyword+'%')).paginate(page, pageSize, False)
+        procurementsItem = procurements.items
+        for row in procurementsItem:
+            db.session.delete(row)
+        db.session.commit()
+        return {
+            "message":"Procurements sucessfully deleted"
         }
-        data.append(temp)
-    return {
-        "page": page,
-        "totalPages": procurements.pages,
-        "data": data
-    }
+        
+    @api.doc(parser=parser)
+    def get(self):
+        page = request.args.get('page', 1, type=int)
+        pageSize = request.args.get('pageSize', 10, type=int)
+        agency = request.args.get('agency','',type=str)
+        supplier = request.args.get('supplier','',type=str)
+        keyword = request.args.get('keyword','',type=str)
+        procurements = Procurements.query.filter(Procurements.agency.like('%'+agency+'%')).filter(Procurements.supplier_name.like('%'+supplier+'%')).filter(Procurements.tender_description.like('%'+keyword+'%')).paginate(page, pageSize, False)
+        procurementsItem = procurements.items
+        data = []
+        for row in procurementsItem:
+            temp = {
+                "tenderNo": row.tender_no,
+                "tenderDescription": row.tender_description,
+                "agency": row.agency,
+                "awardDate": row.award_date.strftime('%Y-%m-%d'),
+                "tenderDetailStatus": row.tender_detail_status,
+                "supplierName": row.supplier_name,
+                "awardedAmt": row.awarded_amt
+            }
+            data.append(temp)
+        return {
+            "page": page,
+            "totalPages": procurements.pages,
+            "data": data
+        }
 
-@app.delete("/procurements")
-def delete_procurements():
-    page = request.args.get('page', 1, type=int)
-    pageSize = request.args.get('pageSize', 10, type=int)
-    agency = request.args.get('agency','',type=str)
-    supplier = request.args.get('supplier','',type=str)
-    keyword = request.args.get('keyword','',type=str)
-    procurements = Procurements.query.filter(Procurements.agency.like('%'+agency+'%')).filter(Procurements.supplier_name.like('%'+supplier+'%')).filter(Procurements.tender_description.like('%'+keyword+'%')).paginate(page, pageSize, False)
-    procurementsItem = procurements.items
-    for row in procurementsItem:
-        db.session.delete(row)
-    db.session.commit()
-    return {
-        "message":"Procurements sucessfully deleted"
-    }
+@api.route('/restore')
+class restore(Resource):
+    def post(self):
+        mycursor = mydb.cursor()
+        sql = "DELETE FROM procurements"
+        mycursor.execute(sql)
+        mydb.commit()
 
-@app.route('/restore', methods=['POST'])
-def restore():
-    mycursor = mydb.cursor()
-    sql = "DELETE FROM procurements"
-    mycursor.execute(sql)
-    mydb.commit()
+        with open('data.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    line_count += 1
+                else:
+                    award_date = datetime.strptime(row[3], '%d/%m/%Y')
+                    award_date = award_date.strftime('%Y-%m-%d')
+                    tender_no = row[0]
+                    tender_description = row[1]
+                    agency = row[2]
+                    tender_detail_status = row[4]
+                    supplier_name = row[5]
+                    awarded_amt = row[6]
 
-    with open('data.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            if line_count == 0:
-                line_count += 1
-            else:
-                award_date = datetime.strptime(row[3], '%d/%m/%Y')
-                award_date = award_date.strftime('%Y-%m-%d')
-                tender_no = row[0]
-                tender_description = row[1]
-                agency = row[2]
-                tender_detail_status = row[4]
-                supplier_name = row[5]
-                awarded_amt = row[6]
+                    mycursor = mydb.cursor()
+                    sql = "INSERT INTO procurements (tender_no, tender_description, agency, award_date, tender_detail_status, supplier_name, awarded_amt) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                    val = (tender_no, tender_description, agency, award_date, tender_detail_status, supplier_name, awarded_amt)
+                    mycursor.execute(sql, val)
+                    mydb.commit()
+                    line_count += 1
+        
+        return {
+            "message": "Procurements sucessfully restored"
+        }
 
-                mycursor = mydb.cursor()
-                sql = "INSERT INTO procurements (tender_no, tender_description, agency, award_date, tender_detail_status, supplier_name, awarded_amt) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-                val = (tender_no, tender_description, agency, award_date, tender_detail_status, supplier_name, awarded_amt)
-                mycursor.execute(sql, val)
-                mydb.commit()
-                line_count += 1
-    
-    return {
-        "message": "Procurements sucessfully restored"
-    }
-
-@app.route('/suppliers', methods=['GET'])
-def suppliers():
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT DISTINCT(supplier_name) FROM procurements ORDER by supplier_name")
-    agencies = mycursor.fetchall()
-    row = []
-    for agency in agencies:
-        row.append(agency[0])
-    return jsonify(row)
+@api.route('/suppliers')
+class suppliers(Resource):
+    def get(self):
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT DISTINCT(supplier_name) FROM procurements ORDER by supplier_name")
+        agencies = mycursor.fetchall()
+        row = []
+        for agency in agencies:
+            row.append(agency[0])
+        return jsonify(row)
 
 
-@app.route('/agencies', methods=['GET'])
-def agencies():
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT DISTINCT(agency) FROM procurements ORDER by agency")
-    agencies = mycursor.fetchall()
-    row = []
-    for agency in agencies:
-        row.append(agency[0])
-    return jsonify(row)
+@api.route('/agencies')
+class agencies(Resource):
+    def get(self):
+        mycursor = mydb.cursor()
+        mycursor.execute("SELECT DISTINCT(agency) FROM procurements ORDER by agency")
+        agencies = mycursor.fetchall()
+        row = []
+        for agency in agencies:
+            row.append(agency[0])
+        return jsonify(row)
 
-app.run()
+if __name__ == '__main__':
+    app.run()
